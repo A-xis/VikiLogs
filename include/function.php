49ee45cfbadd9a0c7315165e3f8b55d3fed1753e
Vivien <A-xis@users.noreply.github.com>
2015-12-02 07:45:58 +0100

    tabulation + 403.2

--- include/function.php
+++ include/function.php
@@ -1,17 +1,17 @@
 <?php
+    defined('_VKL_INIT')||(header("HTTP/1.1 403 Forbidden")&die('403.2 - Read access is denied.'));
 
-function ShowDir($dir = './logs')
-{
-	$handle = opendir($dir);
-    while ($months = readdir($handle))
+    function ShowDir($dir = './logs')
     {
-        // Is there is a way to make this proper?
-        if ($months !== '.') {
-            if ($months !== '..')
-                echo "$months\n";
+        $handle = opendir($dir);
+        while ($months = readdir($handle))
+        {
+            // Is there is a way to make this proper?
+            if ($months !== '.') {
+                if ($months !== '..')
+                    echo "$months\n";
+            }
         }
+        closedir($handle);
     }
-    closedir($handle);
-}
-
 ?>
